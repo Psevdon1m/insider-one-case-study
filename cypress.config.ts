967da304aa3e5f13fdf3c155f7d61f6a829b21e7
Psevdon1m <Valentine.oleynik@gmail.com>
2026-02-15 13:29:11 +0200

    fixed base url

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: "http://localhost:4173",
     specPattern: "tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "tests/e2e/support/e2e.ts",
     fixturesFolder: "tests/e2e/fixtures",
