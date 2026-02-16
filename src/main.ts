ab313a667d78a57bbc7dd2c7512c8f7835713059
Psevdon1m <Valentine.oleynik@gmail.com>
2026-02-16 08:53:49 +0200

    add pwa support ,sw, manifest, icons

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './core/router'
+import { registerSW } from './core/registerSW'
 import './style.css'
 import App from './App.vue'
+
+registerSW()
+
 const pinia = createPinia()
 
 const app = createApp(App)
